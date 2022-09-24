75d00dd5e90e839fa28352887e9badd97de71e4a
danidcode <danieldenizvega@gmail.com>
2022-09-24 16:37:07 +0200

    Navbar completo, faltan retoques

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,42 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.default')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('content')
+<div class="login-container">
+  <div class="layout-login">
+    <div class="login">
+      <div class="login-image">
+        <img src={{ Vite::asset('resources/images/foto_registro.jpg') }}></img>
+      </div>
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-input-label for="name" :value="__('Name')" />
-
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
-                    {{ __('Register') }}
-                </x-primary-button>
-            </div>
+      <div class="login-content">
+        <form method="POST" action="{{ route('register') }}" class="login-content-form">
+          @csrf
+          <div class="login-content-form-group name">
+            <input type="text" class="form-field" placeholder="Nombre" name="name" id="name" required />
+            <label class="form__label">Nombre</label>
+          </div>
+          <div class="login-content-form-group email">
+            <input type="email" class="form-field" placeholder="Email" name="email" id="email" required />
+            <label class="form__label">Email</label>
+          </div>
+          <div class="login-content-form-group password">
+            <input type="password" class="form-field" placeholder="Password" name="password" id="password" required />
+            <label class="form__label">Contraseña</label>
+          </div>
+          <div class="forgotPassword">
+            <Link to="/recovery-password"> <span>¿Contraseña olvidada?</span></Link>
+          </div>
+          <div class="login-content-form-button">
+            <button type="submit" class="login-content-form-button-sign">
+              Crear cuenta
+            </button>
+          </div>
+          <div class="orSignUp"> <a href="/login"> <span>¿Ya tienes cuenta?</span></Link>
+          </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+@endsection
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,16 @@
+<div class="logo">
+    <a href="#">
+        <img src={{ Vite::asset('resources/images/logo_empresa6.png') }} alt="">
+    </a>
+</div>
+
+<div class="navegacion">
+    <input type="checkbox" class="toggle-menu">
+    <div class="burger"></div>
+    <ul class="menu">
+        <li> <a href="#">Home</a></li>
+        <li> <a href="#">Eventos</a></li>
+        <li> <a href="#">Actividades</a></li>
+        <li> <a href="#">Iniciar sesión</a></li>
+    </ul>
+</div>
