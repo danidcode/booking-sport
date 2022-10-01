12927cb223d54eb146db8f5f4aebe7640a533c9d
danidcode <danieldenizvega@gmail.com>
2022-10-01 17:46:37 +0200

    Marcando sección activa en backpanel

--- resources/views/layouts/panel-admin.blade.php
+++ resources/views/layouts/panel-admin.blade.php
@@ -15,18 +15,17 @@
 $routeName = Route::current()->getName()
 @endphp
 
-
 <body>
     <header class="header">
     </header>
     <div id="main" class="main">
         <div class="sidebar">
-            <a class="" href="/dashboard">Home</a>
+            <a class="@if(strpos($routeName, 'dashboard') === 0) active  @endif" href="/dashboard">Home</a>
             <a class="@if(strpos($routeName, 'admin.actividades.') === 0) active  @endif" href="/admin/actividades">Configurar Actividades</a>
-            <a class="" href="/admin/eventos">Configurar Eventos</a>
-            <a class="" href="/admin/reservas">Configurar Reservas</a>
-            <a class="" href="/admin/inscripciones">Configurar Inscripciones</a>
-            <a class="" href="#about">Modificar datos</a>
+            <a class="@if(strpos($routeName, 'admin.eventos.') === 0) active  @endif" href="/admin/eventos">Configurar Eventos</a>
+            <a class="@if(strpos($routeName, 'admin.reservas.') === 0) active  @endif" href="/admin/reservas">Configurar Reservas</a>
+            <a class="@if(strpos($routeName, 'admin.inscripciones.') === 0) active  @endif" href="/admin/inscripciones">Configurar Inscripciones</a>
+            <a class="@if(strpos($routeName, 'admin.modificar-datos.') === 0) active  @endif" href="#about">Modificar datos</a>
             <a class="" href="/">Volver a la página principal</a>
             <a class="" href="#about">Cerrar sesión <i class="fa-solid fa-right-from-bracket"></i></a>
         </div>
