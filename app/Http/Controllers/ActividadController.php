ad36d33da276a0340b463f8a863fb1b4fd9fa406
danidcode <danieldenizvega@gmail.com>
2022-10-02 20:11:05 +0200

    Comenzando a decorar el data table de actividades

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -19,7 +19,21 @@ class ActividadController extends Controller
     }
     public function store(ActividadRequest $request)
     {
-
+        try {
+            $actividad = $request->validated();
+            $imagen = imageInStorage($request->imagen);
+            $actividad['imagen'] = $imagen;
+            Actividad::create($actividad);
+            return response()->json([
+                'status' => true,
+                'message' => 'Actividad creada correctamente',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
     public function show()
     {
@@ -29,8 +43,36 @@ class ActividadController extends Controller
     }
     public function update(ActividadRequest $request, Actividad $actividad)
     {
+
+        try {
+            $actividad = $request->validated();
+            $imagen = imageInStorage($request->imagen);
+            $actividad['image'] = $imagen;
+            $actividad->update($actividad);
+            return response()->json([
+                'status' => true,
+                'message' => 'Actividad actualizada correctamente',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
-    public function destroy()
+    public function destroy(Request $request, Actividad $actividad)
     {
+        try {
+            $actividad->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Actividad borrada correctamente',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 }
--- resources/views/panel-admin/actividades/index.blade.php
+++ resources/views/panel-admin/actividades/index.blade.php
@@ -3,9 +3,105 @@
 @section('content')
 <div class="content">
     <div class="content-wrap">
-    <div class="actividades-wrap">
-        <button class="nueva-actividad-button"> Nueva actividad <i class="fa-solid fa-plus"></i></button>
-    </div>
+        <div class="actividades-wrap">
+            <button class="nueva-actividad-button"> Nueva actividad <i class="fa-solid fa-plus"></i></button>
+
+            <div class="actividades-table-wrap">
+                <table class="actividades-table">
+                    <tr>
+                        <th>Nombre
+                        <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Límite
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Horario
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Destacado
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Destacado principal
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Estatus
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Creada
+                                <i class="fa-solid fa-arrow-up-wide-short"></i>
+                                <i class="fa-solid fa-arrow-down-wide-short"></i>
+                        </th>
+                        <th>Acciones</th>
+                    </tr>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Germany</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td>Mexico</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>Mexico</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Ernst Handel</td>
+                        <td>Austria</td>
+                        <td>Roland Mendel</td>
+                        <td>Austria</td>
+                        <td>Roland Mendel</td>
+                        <td>Austria</td>
+                        <td>Austria</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Island Trading</td>
+                        <td>UK</td>
+                        <td>Helen Bennett</td>
+                        <td>UK</td>
+                        <td>Helen Bennett</td>
+                        <td>UK</td>
+                        <td>UK</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Laughing Bacchus Winecellars</td>
+                        <td>Canada</td>
+                        <td>Yoshi Tannamuri</td>
+                        <td>Canada</td>
+                        <td>Yoshi Tannamuri</td>
+                        <td>Canada</td>
+                        <td>Canada</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                    <tr>
+                        <td>Magazzini Alimentari Riuniti</td>
+                        <td>Italy</td>
+                        <td>Giovanni Rovelli</td>
+                        <td>Italy</td>
+                        <td>Giovanni Rovelli</td>
+                        <td>Italy</td>
+                        <td>Italy</td>
+                        <td class="acciones"><i class="fa-solid fa-ellipsis-vertical"></i></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
