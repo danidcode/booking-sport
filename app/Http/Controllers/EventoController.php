6a462cc7becf636bbdcc5097cb424d92ccfebf08
danidcode <danieldenizvega@gmail.com>
2022-10-25 23:33:02 +0200

    Creando funciones para controlador de eventos y migraciones (falta lógica por desarrollar)

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -2,19 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\EventoRequest;
+use App\Models\Evento;
 use Illuminate\Http\Request;
 
 class EventoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        return view('panel-admin.eventos.index');
     }
+
 
     public function create()
     {
     }
-    public function store()
+    public function store(EventoRequest $request)
     {
+        try {
+            $evento = $request->validated();
+            $imagen = imageInStorage($request->imagen);
+            $evento['imagen'] = $imagen;
+            Evento::create($evento);
+            return response()->json([
+                'status' => true,
+                'message' => 'evento creado correctamente',
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
     public function show()
     {
@@ -27,5 +46,22 @@ class EventoController extends Controller
     }
     public function destroy()
     {
+    }
+
+    public function getEventosJson(Request $request)
+    {
+        $column = $request->column;
+        $order = $request->order;
+        $eventos = Evento::when(isset($order) && isset($column), function ($q) use ($column, $order) {
+            $q->orderBy($column, $order);
+        });
+
+        $eventos = $eventos->paginate(5);
+        if ($request->ajax()) {
+            return response()->json([
+                'status' => true,
+                'eventos' => $eventos,
+            ], 200);
+        }
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Evento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function show(){
+        // $evento_principal = Evento::where('destacado_principal', 1)->first();
         return view('home');
     }
 }
