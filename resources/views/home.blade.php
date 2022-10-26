b1341874dbf30a5287b7f1e316333d93afe20124
danidcode <danieldenizvega@gmail.com>
2022-10-26 22:40:13 +0200

    Texto para el evento y foto preparadp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,12 @@
 <div class="home">
    <div class="home-evento-principal">
       <div class="tarjeta-inscripcion">
+         <div class="home-evento-titulo">
+           <span> Carrera benéfica por Camilo Sánchez </span> 
+         </div>
+         <div class="home-evento-fecha">
+            <span> 20 de noviembre </span> 
+          </div>
          <div> 
                <button class="tarjeta-inscripcion-button"> Inscríbete ya! </button>
          </div>
