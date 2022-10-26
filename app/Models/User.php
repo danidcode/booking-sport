0a69a021c619317dbf610ca968ce533c91b90f8d
danidcode <danieldenizvega@gmail.com>
2022-10-26 22:17:32 +0200

    Estilizando el home

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function reserva(){
+        return $this->hasMany(Reserva::class);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.default')
 @section('content')
 <div class="home">
-   <div class="home-image">
+   <div class="home-evento-principal">
+      <div class="tarjeta-inscripcion">
+         <div> 
+               <button class="tarjeta-inscripcion-button"> Inscríbete ya! </button>
+         </div>
 
+      </div>
+   <img src="storage/gente_corriendo.webp"> </img>
 </div>
 </div>
 @stop
