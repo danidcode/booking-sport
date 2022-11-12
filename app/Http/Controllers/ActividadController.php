012e1722c43930c4c6d6bf6f84d8c5b5a16cd619
danidcode <danieldenizvega@gmail.com>
2022-11-12 14:55:30 +0100

    Primer día en mayúsculas

--- app/Http/Controllers/ActividadController.php
+++ app/Http/Controllers/ActividadController.php
@@ -93,14 +93,14 @@ class ActividadController extends Controller
         $actividades = Actividad::when(isset($order) && isset($column), function ($q) use ($column, $order) {
             $q->orderBy($column, $order);
         })
-        ->withCount(['reserva' => function(Builder $query){
-            $query->where('fecha_reserva', Carbon::now()->toDateString());
-        }]);
-        
+            ->withCount(['reserva' => function (Builder $query) {
+                $query->where('fecha_reserva', Carbon::now()->toDateString());
+            }]);
+
         $actividades = $actividades->paginate(5)->onEachSide(1);
         $actividades->getCollection()->transform(function ($actividad) {
             $actividad->dias_activo = $this->formarDias(json_decode($actividad->dias_activo));
-            
+
             return $actividad;
         });
         if ($request->ajax()) {
@@ -116,7 +116,8 @@ class ActividadController extends Controller
         return view('web.actividades.preview-actividad')->with('actividad', $actividad);
     }
 
-    public function actividadesWeb(){
+    public function actividadesWeb()
+    {
         $actividades = Actividad::where('activo', 1)->paginate(10);
         $actividades->getCollection()->transform(function ($actividad) {
             $actividad->dias_activo = $this->formarDias(json_decode($actividad->dias_activo));
@@ -126,24 +127,26 @@ class ActividadController extends Controller
         return view('web.actividades.listado-actividades')->with('actividades', $actividades);
     }
 
-    public function formarDias($actividad){
+    public function formarDias($actividad)
+    {
         $dias = [];
         $dias_activos = '';
-        $dias[1] = 'Lunes';
-        $dias[2] = 'Martes';
-        $dias[3] = 'Miércoles';
-        $dias[4] = 'Jueves';
-        $dias[5] = 'Viernes';
-        $dias[6] = 'Sábado';
-        $dias[7] = 'Domingo';
+        $dias[1] = 'lunes';
+        $dias[2] = 'martes';
+        $dias[3] = 'miércoles';
+        $dias[4] = 'jueves';
+        $dias[5] = 'viernes';
+        $dias[6] = 'sábado';
+        $dias[7] = 'domingo';
 
         foreach ($actividad as $key => $dia) {
-            if(count($actividad) == $key + 1){
+            if (count($actividad) == $key + 1) {
                 $dias_activos = $dias_activos . ' ' . 'y' . ' ' .  $dias[$dia];
-            }else{
-                $dias_activos = $key != 0 ? ($dias_activos . ', ' . $dias[$dia]) : ($dias_activos . ' ' . $dias[$dia]) ;
+            } else {
+                $dias_activos = $key != 0
+                    ? ($dias_activos . ', ' . $dias[$dia])
+                    : ($dias_activos . ' ' . ucfirst($dias[$dia]));
             }
-            
         }
         return $dias_activos;
     }
