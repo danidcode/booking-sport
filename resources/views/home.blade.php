a28cf5adc32304120aa8dd3da20ea24b0694c4d1
danidcode <danieldenizvega@gmail.com>
2022-11-17 19:17:07 +0100

    Responsive del carrusel y algo de lógica en el blade del home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,44 @@
 @extends('layouts.default')
 @section('content')
     <div class="home">
-        @if(isset($evento_principal))
-        <div class="home-evento-principal">
-            <div class="tarjeta-inscripcion">
-                <div class="home-evento-titulo">
-                    <span> {{$evento_principal->nombre}} </span>
-                </div>
-                <div class="home-evento-fecha">
-                    <span> {{$evento_principal->fecha_inicio}} </span>
-                </div>
-                <div>
-                    <a href="inscripcion-evento/{{$evento_principal->id}}" class="tarjeta-inscripcion-button"> Inscríbete ya! </a>
-                </div>
-
-
-            </div>
-            <img src="{{$evento_principal->imagen}}"> </img>
-        </div>
-        @endif
-        <div class="home-actividades-destacadas">
-            <div class="home-actividades-destacadas-text">
-                <span> Actividades destacadas</span>
-            </div>
-            @include('components/carrusel',  ['actividades' => $actividades])
-        </div>
-        
-
-        <div class="home-eventos-destacados">
-            <div class="home-eventos-destacados-text">
-                <span> Eventos destacados</span>
-            </div>
-
-            <div class="swiper home-swiper">
-                <div class="swiper-wrapper">
-                    @foreach ($eventos as $evento)
-                    <div class="swiper-slide">
-                        <img src="{{ $evento->imagen }}" alt=""> </img>
-                        <div class="swiper-slide-actividad-content">
-                            <span> {{ $evento->nombre }}</span>
-                            <span> {{$evento->fecha_inicio}}</span>
-                            <div> <a href="inscripcion-evento/{{$evento->id}}" class="tarjeta-inscripcion-button"> Inscribirse </a></div>
-                        </div>
+        @if (isset($evento_principal))
+            <div class="home-evento-principal">
+                <div class="tarjeta-inscripcion">
+                    <div class="home-evento-titulo">
+                        <span> {{ $evento_principal->nombre }} </span>
                     </div>
-                    @endforeach
+                    <div class="home-evento-fecha">
+                        <span> {{ $evento_principal->fecha_inicio }} </span>
+                    </div>
+                    <div>
+                        <a href="inscripcion-evento/{{ $evento_principal->id }}" class="tarjeta-inscripcion-button">
+                            Inscríbete ya! </a>
+                    </div>
+
+
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+                <img src="{{ $evento_principal->imagen }}"> </img>
             </div>
-        </div>
+        @endif
+
+        @if ($actividades->isNotEmpty())
+            <div class="home-actividades-destacadas">
+                <div class="home-actividades-destacadas-text">
+                    <span> Actividades destacadas</span>
+                </div>
+                @include('components/carrusel', ['actividades' => $actividades])
+            </div>
+        @endif
+
+        @if ($eventos->isNotEmpty())
+            <div class="home-eventos-destacados">
+                <div class="home-eventos-destacados-text">
+                    <span> Eventos destacados</span>
+                </div>
+                @include('components/carrusel', ['eventos' => $eventos])
+            </div>
     </div>
+    @endif
 @stop
 
 @section('scripts')
